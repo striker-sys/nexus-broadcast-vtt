30917c829ef735624666a06c85f4701c420a769b
striker-sys <striker@zocker-freun.de>
2026-02-17 21:20:32 +0100

    Create Center-Text.js

---
+++ Skripts/Center-Text.js
@@ -0,0 +1,23 @@
+if (!game.user.isGM) return ui.notifications.warn("Nur der GM kann senden.");
+
+const sock = game["nexus-overlay_socket"];
+if (!sock) return ui.notifications.error("Socket nicht bereit. Foundry neu starten?");
+
+const text = `
+<h2>Warnung</h2>
+<p><i>Die Luft wird kalt…</i></p>
+<p><b>Roll Initiative!</b></p>
+`;
+
+await sock.executeForEveryone("showCenter", {
+  html: text,
+
+  displaySeconds: 6,      // Anzeige zeit in Sekunden
+  width: "50%",           // Breite der Anzeige im verhältniss zum Bildschirm
+  fontSize: 44,           // Schriftgröße
+
+  fadeInMs: 200,          // Einblende Zeit in Millisekunden.
+  fadeOutMs: 900,         // Ausblende zeit in Millisekunden.
+
+  background: "transparent" // z.B. "rgba(0,0,0,0.35)"
+});
